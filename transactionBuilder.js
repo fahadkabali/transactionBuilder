297690be208f8e38e26fea16dfdf653dd9c0f976
fahadkabali <kabalifahad@gmail.com>
2024-02-08 21:07:38 +0300

    changed the preimage script

--- transactionBuilder.js
+++ transactionBuilder.js
@@ -1,38 +1,39 @@
-import crypto from 'crypto'
-import bitcoin from 'bitcoinjs-lib'
-import { ECPairFactory } from 'ecpair'
-import * as ecc from 'tiny-secp256k1'
+const bitcoin = require('bitcoinjs-lib');
+const ECPairFactory = require('ecpair').default;
+const ecc = require('tiny-secp256k1');
+const ECPair = ECPairFactory(ecc);
+const network = bitcoin.networks.testnet;
 
-const ECPair = ECPairFactory(ecc)
+// Step 1: Computing SHA-256 hash of the string "Btrust Builders"
+const preimageString = 'Btrust Builders';
+const sha256HashString = bitcoin.crypto.sha256(Buffer.from(preimageString, 'utf-8')).toString('hex');
 
-// Function for generating the redeem script in hex format for the given pre-image.
-function generateRedeemScript(preImage) {
-  const sha256Hash = crypto.createHash('sha256').update(preImage).digest('hex')
-  const redeemScript = `OP_SHA256 ${sha256Hash} OP_EQUAL`
-  return redeemScript
-}
+console.log('SHA-256 hash of the preimage:', sha256HashString);
 
-const preImage = 'Btrust Builders'
-const redeemScript = generateRedeemScript(preImage)
-//console.log(redeemScript)
+// Step 2: Format the redeem script to hexadecimal representation
+const redeemScript = bitcoin.script.compile([
+  bitcoin.opcodes.OP_SHA256,
+  Buffer.from(sha256HashString, 'hex'),
+  bitcoin.opcodes.OP_EQUAL,
+]);
+console.log('redeemScript Hex: ', redeemScript.toString('hex'))
+console.log('redeemScript ASM:', bitcoin.script.toASM(redeemScript))
 
-// Function for deriving an address with from the above redeem script.
-function deriveAddress(redeemScript) {
-  const script = bitcoin.script.fromASM(redeemScript)
-  const { address } = bitcoin.payments.p2sh({ redeem: { output: script } })
-  return address
-}
-const address = deriveAddress(redeemScript);
-//console.log(address);
+const redeemScriptBuffer = Buffer.from(redeemScript, 'hex')
+
+// Derive P2SH address
+const outputAddress = bitcoin.payments.p2sh({
+  redeem: { output: redeemScriptBuffer },
+}).address;
+console.log('Derived P2SH Address:', outputAddress)
 
 // Function to construct a transaction.
-function constructTransaction(address, amount) {
-  const network = bitcoin.networks.testnet
-  const keyPair = ECPair.makeRandom({ network });
-  //const privateKey = Buffer.from('16e05614526c1ebd3a170a430a1906a6484fdd203ab7ce6690a54938f5c44d7d', 'hex');
-  //const keyPair = ECPair.fromPrivateKey(privateKey, { network })
+function constructTransaction(outputAddress, amount) {
+  //const keyPair = ECPair.makeRandom({ network });
+  const privateKey = Buffer.from(sha256HashString, 'hex')
+  const keyPair = ECPair.fromPrivateKey(privateKey, { network })
 
-  const psbt = new bitcoin.Psbt(bitcoin.networks.testnet)
+  const psbt = new bitcoin.Psbt({network:network})
 
   psbt.addInput({
     hash: 'ff272fdb1e9f2ca6f7d286b4b0a7969237d65cc5b9cbe196f1c95f8b6e860507',
@@ -40,14 +41,14 @@ function constructTransaction(address, amount) {
     nonWitnessUtxo: Buffer.from(
       '02000000000101a31ed7920359264286a246deff28260f73a35062de0b8b65a3a3d9ddfd59acf90000000000fdffffff02c287caf4000000001976a91499cad6021512d7bd2078c3f47922888832ca5c3d88ac20030300000000001976a914a6dc9efbbd50e1ef0c3ac6763228bc96d9adce8388ac024730440220499ba5fde8e69118ccc93f428a994e4b56ae551663ae02b51466857e82e32f1502206d4fc453e95a0ab4e0fdfdf38abd389ba042449ce98e78336f63391905ea5f8b0121035468dabd4848979d155100226a47e378a0270b449a16078c3b2c2f6baa2b10b431522700',
       'hex'
-    ),
+    )
   });
 
   psbt.addOutput({
-    address: address,
+    script: Buffer.from(outputAddress, 'hex'),
     value: Math.floor(amount * 1e8),
   })
-  psbt.signInput(0, keyPair)
+  psbt.signInput(1, keyPair)
   psbt.finalizeAllInputs()
 
   const txHex = psbt.extractTransaction().toHex()
@@ -55,7 +56,7 @@ function constructTransaction(address, amount) {
 }
 
 const amount = 0.001
-const transactionHex = constructTransaction(address, amount)
+const transactionHex = constructTransaction(outputAddress, amount)
 console.log(transactionHex)
 
 
